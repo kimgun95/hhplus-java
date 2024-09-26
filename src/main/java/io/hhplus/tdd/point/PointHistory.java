96406b871f01cf40ccdbd0e96bd4e04aee28a589
kimgun95 <harrison1995@naver.com>
2024-09-27 01:04:42 +0900

    feat: 엔티티에 검증 로직, 정적 팩터리 메서드 구현 정적 팩터리 메서드를 구현한 이유 - 유연하게 객체 생성을 하고 싶었다
    검증 로직을 넣은 이유
    UserPoint에서 point에 대해 검증을 하는 책임을 주고 싶었다.
    
    TO-DO : UserPoint에서 포인트 충전/이용시 이용될 포인트 add/sub 기능을 만들어 비즈니스 로직을 추가하기

--- src/main/java/io/hhplus/tdd/point/PointHistory.java
+++ src/main/java/io/hhplus/tdd/point/PointHistory.java
@@ -7,4 +7,14 @@ public record PointHistory(
         TransactionType type,
         long updateMillis
 ) {
+
+    public static PointHistory of(
+            long id,
+            long userId,
+            long amount,
+            TransactionType type,
+            long updateMillis
+    ) {
+        return new PointHistory(id, userId, amount, type, updateMillis);
+    }
 }
--- src/main/java/io/hhplus/tdd/point/UserPoint.java
+++ src/main/java/io/hhplus/tdd/point/UserPoint.java
@@ -1,10 +1,28 @@
 package io.hhplus.tdd.point;
 
+import io.hhplus.tdd.exception.RestApiException;
+import io.hhplus.tdd.exception.UserPointErrorCode;
+
 public record UserPoint(
         long id,
         long point,
         long updateMillis
 ) {
+
+    public UserPoint {
+        point = validate(point);
+    }
+
+    public long validate(long point) {
+        if (point < 0) {
+            throw new RestApiException(UserPointErrorCode.NOT_ENOUGH_BALANCE);
+        }
+        return Math.min(point, 100_000);
+    }
+
+    public static UserPoint of(long id, long point, long updateMillis) {
+        return new UserPoint(id, point, updateMillis);
+    }
 
     public static UserPoint empty(long id) {
         return new UserPoint(id, 0, System.currentTimeMillis());
