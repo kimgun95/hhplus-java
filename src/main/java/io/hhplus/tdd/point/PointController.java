2ad0b47389a0471aaa614fa817c51a0fc7af2761
kimgun95 <harrison1995@naver.com>
2024-09-27 00:53:09 +0900

    feat: 포인트 조회,충전,이용 관련한 api를 담당하는 컨트롤러 구현 응답값에 ResponseEntity를 사용한 이유 - Restful API 설계에 맞는 구조로 상태코드,헤더,본문을 명확하게 설정할 수 있기 때문이다. - 클라이언트와 명확한 커뮤니케이션을 가능하게 하기 때문이다.

--- src/main/java/io/hhplus/tdd/point/PointController.java
+++ src/main/java/io/hhplus/tdd/point/PointController.java
@@ -1,56 +1,66 @@
 package io.hhplus.tdd.point;
 
+import io.hhplus.tdd.constant.Id;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/point")
 public class PointController {
 
     private static final Logger log = LoggerFactory.getLogger(PointController.class);
+    private final PointService pointService;
 
     /**
      * TODO - 특정 유저의 포인트를 조회하는 기능을 작성해주세요.
      */
     @GetMapping("{id}")
-    public UserPoint point(
+    public ResponseEntity<UserPoint> point(
             @PathVariable long id
     ) {
-        return new UserPoint(0, 0, 0);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(pointService.searchUserPoint(new Id<>(UserPoint.class, id)));
     }
 
     /**
      * TODO - 특정 유저의 포인트 충전/이용 내역을 조회하는 기능을 작성해주세요.
      */
     @GetMapping("{id}/histories")
-    public List<PointHistory> history(
+    public ResponseEntity<List<PointHistory>> history(
             @PathVariable long id
     ) {
-        return List.of();
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(pointService.searchUserPointHistory(new Id<>(UserPoint.class, id)));
     }
 
     /**
      * TODO - 특정 유저의 포인트를 충전하는 기능을 작성해주세요.
      */
     @PatchMapping("{id}/charge")
-    public UserPoint charge(
+    public ResponseEntity<UserPoint> charge(
             @PathVariable long id,
             @RequestBody long amount
     ) {
-        return new UserPoint(0, 0, 0);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(pointService.chargeUserPoint(new Id<>(UserPoint.class, id), amount));
     }
 
     /**
      * TODO - 특정 유저의 포인트를 사용하는 기능을 작성해주세요.
      */
     @PatchMapping("{id}/use")
-    public UserPoint use(
+    public ResponseEntity<UserPoint> use(
             @PathVariable long id,
             @RequestBody long amount
     ) {
-        return new UserPoint(0, 0, 0);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(pointService.useUserPoint(new Id<>(UserPoint.class, id), amount));
     }
 }
